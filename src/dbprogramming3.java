80c9d7da3d5cb2ac575df5011a5374583f6d6a50
Nisarat-A <noansrnn@gmail.com>
2023-05-02 15:17:17 +0700

    -

--- src/dbprogramming3.java
+++ src/dbprogramming3.java
@@ -9,7 +9,7 @@ public class dbprogramming3 {
     public static void main(String[] args) {
         String databaseURL = "jdbc:mysql://127.0.0.1:3306/int103_may2";
         String username = "root" ;
-        String password = "   ";
+        String password = "";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("driver is loaded");
@@ -19,7 +19,7 @@ public class dbprogramming3 {
             String sql = "select * from Student";
             ResultSet result = statement.executeQuery(sql);
             while (result.next()){
-                System.out.println("Hello");
+            System.out.println(result.getString(1) + " " + result.getString(2) + " " + result.getString(3) + " " + result.getString(4));
             }
         } catch (ClassNotFoundException e) {
             Logger.getLogger(dbprogramming3.class.getName()).log(Level.SEVERE, null, e);
