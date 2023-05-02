9c7c90d934cd71c7b39b0ceb8c6df578cd2a43d2
Nisarat-A <noansrnn@gmail.com>
2023-05-02 15:08:35 +0700

    -

---
+++ src/dbprogramming3.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+import java.sql.DriverManager;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+
+public class dbprogramming3 {
+    public static void main(String[] args) {
+        String databaseURL = "jdbc:mysql://127.0.0.1:3306/int103_may2";
+        String username = "root" ;
+        String password = "   ";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("driver is loaded");
+            Connection con = DriverManager.getConnection(databaseURL, username, password);
+            System.out.println("database connected");
+            Statement statement = con.createStatement();
+            String sql = "select * from Student";
+            ResultSet result = statement.executeQuery(sql);
+            while (result.next()){
+                System.out.println("Hello");
+            }
+        } catch (ClassNotFoundException e) {
+            Logger.getLogger(dbprogramming3.class.getName()).log(Level.SEVERE, null, e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
